91ce698e89bf7943c60ffff22e34834f645fa3d3
Desh Deepak <51161053+darshdesh@users.noreply.github.com>
2019-12-15 21:30:02 +0530

    new entry autowebbrowser
    quick reuse

---
+++ auto_webbrowser.py
@@ -0,0 +1,9 @@
+import os
+import webbrowser
+webbrowser.open_new_tab('https://hpe-psms.in/Account/SignIn')
+webbrowser.open_new_tab('http://hrone.ciinfotech.net/ciinfotech')
+webbrowser.open_new_tab('https://sbpdcl.co.in/')
+
+
+
+'''os.system("killall -9 'firefox'")'''
